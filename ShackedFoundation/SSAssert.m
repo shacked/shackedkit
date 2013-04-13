51a39fd79dddb4df4faf81b5c44d27763e79e075
Dave Keck <dave@getshacked.com>
2013-04-12 21:48:16 -1000

    Updated SSAssert to provide more meaningful reasons for raised NSExceptions.

--- ShackedFoundation/SSAssert.m
+++ ShackedFoundation/SSAssert.m
@@ -3,22 +3,22 @@
 
 void SSAssertHandle(const char *file, uintmax_t line, const char *function, const char *assertion, const char *note, BOOL raiseException)
 {
-    NSString *message = [NSString stringWithFormat: @"=== Assertion failed ===\n  Time: %@\n", [[NSDate date] description]];
+    NSMutableString *message = [NSMutableString stringWithFormat: @"=== Assertion failed ===\n  Time: %@\n", [[NSDate date] description]];
     
     if (file)
-        message = [message stringByAppendingFormat: @"  File: %@:%ju\n", [[NSString stringWithUTF8String: file] lastPathComponent], line];
+        [message appendFormat: @"  File: %@:%ju\n", [[NSString stringWithUTF8String: file] lastPathComponent], line];
     
     if (function)
-        message = [message stringByAppendingFormat: @"  Function: %@\n", [NSString stringWithUTF8String: function]];
+        [message appendFormat: @"  Function: %@\n", [NSString stringWithUTF8String: function]];
     
     if (assertion)
-        message = [message stringByAppendingFormat: @"  Assertion: %@\n", [NSString stringWithUTF8String: assertion]];
+        [message appendFormat: @"  Assertion: %@\n", [NSString stringWithUTF8String: assertion]];
     
     if (note)
-        message = [message stringByAppendingFormat: @"\n  ### %@\n", [NSString stringWithUTF8String: note]];
+        [message appendFormat: @"\n  ### %@\n", [NSString stringWithUTF8String: note]];
     
     fprintf(stderr, "%s", [message UTF8String]);
     
     if (raiseException)
-        [NSException raise: NSInternalInconsistencyException format: @"Assertion failed"];
+        [NSException raise: NSInternalInconsistencyException format: message];
 }
